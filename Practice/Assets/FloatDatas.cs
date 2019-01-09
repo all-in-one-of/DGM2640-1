70b0943a5d7da1899b3fd72ab8ead25eee216317
Meaghan Searle <msearle88@gmail.com>
2019-01-09 10:53:33 -0800

    Practiceproject

---
+++ Practice/Assets/FloatDatas.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewFloat")]
+
+
+
+public class FloatDatas : ScriptableObject
+
+
+
+{
+
+
+
+    public float value; 
+
+
+
+    public virtual float Value
+
+
+
+    {
+
+
+
+        get { return value;}
+
+
+
+		
+
+
+
+    }
+
+
+
+
+
+
+
+}
---
+++ Practice/Assets/MoveBases.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    [RequireComponent(typeof(CharacterController))]
+
+
+
+    public class MoveBases : MonoBehaviour
+
+
+
+    {
+
+
+
+        private CharacterController controller;
+
+        public MovePatterns MovePatterns;
+
+
+
+        private void Start()
+
+
+
+        {
+
+
+
+            controller = GetComponent<CharacterController>();
+
+
+
+        }
+
+
+
+        void Update()
+
+
+
+        {
+
+
+
+            MovePatterns.Invoke(controller, transform);
+
+
+
+        }
+
+
+
+    }

a6f4f53e08f71d949699b3890737208e03751310
Meaghan Searle <msearle88@gmail.com>
2019-01-09 11:32:24 -0800

    Carry script

---
+++ Practice/Assets/Carry.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class Carry : MonoBehaviour
+{
+    public GameObject box;
+    public GameObject newParent;
+    public Input pickup; 
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (Input.GetButton("PickUp"))
+        {
+            box.transform.parent = newParent.transform; 
+        }
+    }
+}

dd14f5c457a7887ffcd97970836e8a8e3a6fe0f9
Meaghan Searle <msearle88@gmail.com>
2019-01-12 11:17:13 -0800

    inputmatch and scriptable string object

---
+++ Practice/Assets/InputMatch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputMatch : MonoBehaviour
+{
+    public TextForMatch match;
+    public InputField input;
+    public FloatDatas score; 
+
+    public void Update()
+    {
+        if (input.textComponent == match)
+        {
+            score.value = score.value + 1;
+            Debug.Log("Running");
+        }
+    }
+    
+}
---
+++ Practice/Assets/TextForMatch.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class TextForMatch : ScriptableObject
+{
+    public String text; 
+}

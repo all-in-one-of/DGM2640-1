9b9a9332a841293c3ebe4e128e1c6c8f46e5e03e
Meaghan Searle <msearle88@gmail.com>
2019-01-14 22:57:20 -0800

    choice switch

---
+++ Practice/Assets/ChoiceTest.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class ChoiceTest : MonoBehaviour
+{
+
+
+    public bool haskey = false;
+    public FloatDatas danger;
+    public bool gateway; 
+
+
+
+    public enum playerchoices
+
+    {
+
+        street,
+
+        close,
+
+        road,
+
+        gate,
+
+    };
+
+
+
+    public playerchoices choice;
+
+
+
+
+    void OnTriggerEnter(Collider other)
+
+
+
+    {
+
+        if (other.gameObject.CompareTag("Player"))
+
+        {
+
+
+
+            switch (choice)
+
+            {
+
+                case (playerchoices.close):
+
+                    danger.value = danger.value + 5;
+
+                   
+
+                    break;
+
+				
+
+                case (playerchoices.road):
+
+                    haskey = true;
+                    danger.value = danger.value + 2;
+
+           
+
+                    break; 
+
+				
+
+                case (playerchoices.street):
+
+                    danger.value = danger.value - 2;
+
+                    break;
+
+					
+
+                case (playerchoices.gate):
+
+                    danger.value = danger.value + 1;
+                    if (haskey = true)
+                    {
+                        gateway = true; 
+                    }
+                    
+                    break;
+
+
+
+
+
+            }
+
+        }
+
+
+
+    }
+
+}
+
+
+
+
+
+
+

315ee9578e0bb2f28864e943e01b87b21378c64f
Meaghan Searle <msearle88@gmail.com>
2019-01-17 11:06:14 -0700

    created a new project because it wasn't opening in my original

---
+++ PracticeAgain/Assets/SwitchPosition.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class SwitchPosition : MonoBehaviour {
+
+	public GameObject[] box;
+    
+    public Vector3[] newPosition;
+
+	public bool CanChange = true; 
+  
+	    private GameObject getBox()
+    
+    	{
+    
+    		return box[Random.Range(0, 4)];
+    
+    	}
+    
+    	private Vector3 getNewPosition()
+    	{
+    		return newPosition[Random.Range(0, 9)];
+    	}
+    
+    
+    	IEnumerator Start()
+	   
+    	{
+
+		    while (CanChange)
+		    {
+			    yield return new WaitForSeconds(Random.Range(1, 3));
+                getBox().transform.position = new Vector3(getNewPosition().x, getNewPosition().y, getNewPosition().z);
+		    }
+
+		    yield break;
+
+	    }
+}

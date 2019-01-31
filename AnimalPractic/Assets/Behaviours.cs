7f89d1ce202e6251449bbe25eafc082514ce0714
Meaghan Searle <msearle88@gmail.com>
2019-01-31 12:59:44 -0700

    animal project lecture

---
+++ AnimalPractic/Assets/Behaviours.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Behaviours : MonoBehaviour
+{
+
+
+	public UnityEvent OnAwake, OnStart, TriggerEnter, CollisionEnter; 
+
+	
+
+	private void Awake()
+	{
+		OnAwake.Invoke();
+	}
+
+	private void Start()
+	{
+		OnStart.Invoke();
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		TriggerEnter.Invoke();
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		CollisionEnter.Invoke();
+	}
+
+	private void OnDestroy()
+	{
+		
+	}
+}

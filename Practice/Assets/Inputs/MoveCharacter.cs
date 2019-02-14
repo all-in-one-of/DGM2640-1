527f4f766c2376a65989f6f9b1414d158365e4d2
Meaghan Searle <msearle88@gmail.com>
2019-02-14 12:54:33 -0700

    character controller lecture

---
+++ Practice/Assets/Inputs/MoveCharacter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MoveCharacter : MonoBehaviour
+{
+    private CharacterController cc;
+    private Vector3 position;
+    public FloatDatas speed;
+    public FloatDatas gravity; 
+         
+         void Start()
+         {
+             cc = GetComponent<CharacterController>();
+         }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position.x = Input.GetAxis("Horizontal") * Time.deltaTime * speed.value;
+        if (cc.isGrounded)
+        {
+            if (Input.GetButton("Jump"))
+            {
+                position.y += 100;
+            }
+        }
+        position.y = gravity.value * Time.deltaTime;
+        cc.Move(position);
+    }
+}

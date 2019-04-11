32d0ff251ab6c03f09dc2f90bc2d92c5c6f6c23a
Meaghan Searle <msearle88@gmail.com>
2019-04-11 13:11:09 -0600

    still trying to get the flock to work

--- Practice/Assets/EachBirdFish.cs
+++ Practice/Assets/EachBirdFish.cs
@@ -10,9 +10,9 @@ public class EachBirdFish : MonoBehaviour
     private Vector3 averageHeading;
     private Vector3 averagePosition;
     public float neighborDistance;
-    public float tankSize; 
+    public float tankSize;
 
-    private bool turning = false;
+    private bool turning = true;
 
     public Transform goal;
     
@@ -25,13 +25,13 @@ public class EachBirdFish : MonoBehaviour
     void Update()
     {
 
-        if (Vector3.Distance(transform.position, Vector3.zero) >= tankSize)
+     /*   if (Vector3.Distance(transform.position, Vector3.zero) >= tankSize)
         {
             turning = true;
         }
 
         else turning = false;
-
+*/
         if (turning)
         {
             Vector3 direction = Vector3.zero - transform.position;
@@ -44,17 +44,29 @@ public class EachBirdFish : MonoBehaviour
             
         transform.Translate(0, 0, Time.deltaTime * speed);
     }
+    
+    private void  FixedUpdate()
+
+    {
+
+        Vector3 localPosition = goal.transform.position - transform.position;
+
+        localPosition = localPosition.normalized;
+
+        transform.Translate(localPosition.x * Time.deltaTime * speed, localPosition.y * Time.deltaTime * speed, localPosition.z * Time.deltaTime * speed);
+
+    }
 
    private void ApplyRules()
     {
         GameObject[] gos;
-        gos = BirdList.FindObjectsOfType<GameObject>(); 
+        gos = FindObjectsOfType<GameObject>(); 
         
-        Vector3 vcentre = Vector3.zero;
+        //Vector3 vcentre = Vector3.zero;
         Vector3 vavoid = Vector3.zero;
         float gSpeed = 0.1f;
 
-        //VectorSo goal = Flock.FindObjectOfType<VectorSo>() ;
+        VectorSo goal = FindObjectOfType<VectorSo>() ;
 
         float dist; 
 
@@ -65,12 +77,12 @@ public class EachBirdFish : MonoBehaviour
             dist = Vector3.Distance(go.transform.position, transform.position);
             if (dist <= neighborDistance)
             {
-                vcentre += go.transform.position;
+               // vcentre += go.transform.position;
                 groupSize++;
 
                 if (dist < 1.0f)
                 {
-                    vavoid = vavoid + (transform.position - go.transform.position);
+                    vavoid = vavoid - (transform.position + go.transform.position);
                 }
 
                 EachBirdFish anotherFlocking = go.GetComponent<EachBirdFish>();
@@ -80,17 +92,17 @@ public class EachBirdFish : MonoBehaviour
 
         if (groupSize > 0)
         {
-            //vcentre = vcentre / groupSize + (goal.VectorData - this.transform.position);
+           // vcentre = vcentre / groupSize + (goal.VectorData - this.transform.position);
             speed = gSpeed / groupSize;
 
-            Vector3 direction = (vcentre + vavoid) - transform.position;
+            Vector3 direction = goal.VectorData.normalized;
 
-            if (direction != Vector3.zero)
+            if (direction != goal.VectorData)
                 transform.rotation = Quaternion.Slerp(transform.rotation,
                     Quaternion.LookRotation(direction),
                     rotationspeed * Time.deltaTime);
         }
+        
     }
-    
     
 }
---
+++ Practice/Assets/SwitchOnePlace.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchOnePlace : MonoBehaviour
+{
+    public GameObject goal;
+
+    public Vector3[] newPosition;
+    public bool CanChange = true;
+    
+    private Vector3 getNewPosition()
+    {
+        return newPosition[Random.Range(0, 6)];
+    }
+    
+    IEnumerator Start()
+    {
+        while (CanChange)
+        {
+            yield return new WaitForSeconds(Random.Range(1, 5));
+            goal.transform.localPosition = new Vector3(getNewPosition().x, getNewPosition().y, getNewPosition().x); 
+        } 
+        yield break;
+
+    }
+}
